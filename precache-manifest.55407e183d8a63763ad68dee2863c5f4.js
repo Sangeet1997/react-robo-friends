0c156c3dd64d060470cb93b30b5ea0b2be0b569d
Sangeet Saha <Sangeet1997@users.noreply.github.com>
2020-05-20 14:23:54 +0530

    Updates

---
+++ precache-manifest.55407e183d8a63763ad68dee2863c5f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e215b7c9f2ca9f8c5fdb5428f2d51f69",
+    "url": "/react-robo-friends/index.html"
+  },
+  {
+    "revision": "38d36c6b65548b0ac965",
+    "url": "/react-robo-friends/static/css/main.c937cbf3.chunk.css"
+  },
+  {
+    "revision": "c276127bd656ae42a059",
+    "url": "/react-robo-friends/static/js/2.fc20ba68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-robo-friends/static/js/2.fc20ba68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d36c6b65548b0ac965",
+    "url": "/react-robo-friends/static/js/main.5bdd92e9.chunk.js"
+  },
+  {
+    "revision": "c4b2b6f863f927214bcf",
+    "url": "/react-robo-friends/static/js/runtime-main.20ea63cc.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
